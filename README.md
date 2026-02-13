cee18c03d89ab902055b854a4cabbec91da7a855
Alex Fernando Demeza Hernandez <kzshop2024@gmail.com>
2026-02-12 19:29:59 -0600

    Add README and initial index.php file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # test1_alexdemeza
 Repositorio para demostrar mis skills trabajando con git
+
+## Institucion educativa
+
+Universidad Tecnologixa de la Selva
+
+## Lugar de procedencia
+
+Ocosingo, Chiapas. 2026
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Alex's page</title>
+</head>
+<body>
+    Bien venido a la página de Alex xdxd
+</body>
+</html>
