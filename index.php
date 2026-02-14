4dc110a32605e69fe1675b08dfad5624b05474f0
Alex Fernando Demeza Hernandez <kzshop2024@gmail.com>
2026-02-13 18:36:25 -0600

    Add favicon.svg and link it in index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alex's page</title>
+    <link rel="icon" type="image/svg+xml" href="favicon.svg">
 </head>
 <body>
     Bien venido a la página de Alex xdxd
